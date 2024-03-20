592e61291282b359d9634ba28b60b046c01a33e2
dongchenys <14018478+dongchenys@user.noreply.gitee.com>
2024-03-20 06:16:12 +0000

    update js/zhongzi.js.
    Signed-off-by: dongchenys <14018478+dongchenys@user.noreply.gitee.com>

--- js/zhongzi.js
+++ js/zhongzi.js
@@ -7,7 +7,7 @@ var rule = {
     url: '/api.php/provide/vod?ac=detail&t=fyclass&pg=fypage&f=',
     class_name: '短剧&KS&DY&都市&古装&重生&逆袭&虐恋&萌宝&言情&穿越&战神&神医&赘婿&甜宠&其他',
     class_url: '1&2&3&4&5&6&7&8&9&10&11&12&13&14&15&16',
-    tab_rename:{'zzdj':'东辰影视'}, 
+    tab_rename:{'zzdj':'东辰影视2群：783264601'}, 
     推荐: 'json:list;vod_name;vod_pic;vod_remarks;vod_id',
     一级: 'json:list;vod_name;vod_pic;vod_remarks;vod_id',
     二级: `js:
